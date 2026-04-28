5c33093b7a4b63deb0af45a88ee3d8aa5325f028
Sujalj9146 <sujalj9146@gmail.com>
2026-04-29 01:10:28 +0530

    Delete src/main directory

--- src/main/java/com/expense/repository/ExpenseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.expense.repository;
-
-import com.expense.model.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-}
